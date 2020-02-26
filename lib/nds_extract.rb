ed23f9c39c38aaf56a2a2ebcacb3e4a6b386186c
Daniel Andrew <dwandrew@hotmail.com>
2020-02-26 07:57:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -68,7 +68,7 @@ def gross_per_studio(collection)
 index = 0
 gross_array = []
 while index<collection.length
-if gross_array.includes?(collection[index][:studio])
+if gross_array.include?(collection[index][:studio])
   gross_array[collection[index][:studio]] += collection[index][:worldwide_gross]
 else gross_array.push(collection[index][:studio] => collection[index][:worldwide_gross])
 end
